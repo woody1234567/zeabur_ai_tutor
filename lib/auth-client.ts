ec76940dc26cd61b6dbd50d3f451d875b19f9cd0
woody <johannes8873@gmail.com>
2025-11-27 08:41:32 +0800

    fix:second try to solve the deployment problem(base_url)

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -3,7 +3,9 @@ import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   baseURL: import.meta.env.PROD
-    ? "https://ai-tutor-new-seven.vercel.app"
+    ? typeof window !== "undefined"
+      ? window.location.origin
+      : "https://ai-tutor-new-seven.vercel.app"
     : "http://localhost:3000",
   plugins: [adminClient()],
 });
