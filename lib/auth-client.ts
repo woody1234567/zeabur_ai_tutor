992cb3c38c7e3c4cf961e36ada5f389a9ad3e919
woody <johannes8873@gmail.com>
2025-12-01 10:51:21 +0800

    feat:update AuthClient

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,11 +1,29 @@
 import { createAuthClient } from "better-auth/vue";
 import { adminClient } from "better-auth/client/plugins";
+const getBaseURL = () => {
+  if (typeof window !== "undefined") {
+    return window.location.origin;
+  }
+
+  // Server-side logic (SSG, SSR)
+  const env = process.env.VERCEL_ENV;
+
+  if (env === "production") return "https://preview.studywithwoody.site";
+  if (env === "preview") return "https://preview.studywithwoody.site";
+
+  return "http://localhost:3000";
+};
 
 export const authClient = createAuthClient({
-  baseURL: process.client
-    ? window.location.origin
-    : (useRuntimeConfig().public.betterAuthUrl as string),
+  baseURL: getBaseURL(),
   plugins: [adminClient()],
 });
+
+// export const authClient = createAuthClient({
+//   baseURL: process.client
+//     ? window.location.origin
+//     : (useRuntimeConfig().public.betterAuthUrl as string),
+//   plugins: [adminClient()],
+// });
 
 export const { signIn, signOut, useSession } = authClient;
